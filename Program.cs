04cbdcf90b77e10452c04232e3dc7318efe5fe81
Siddhant <santoshshrivastava39@gmail.com>
2022-03-24 23:25:21 +0530

    Call in main methd

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<int> binarytree = new BinaryTree<int>(56);
+            binarytree.Insert(30);
+            binarytree.Insert(70);
+            Console.WriteLine("Binary Search Tree ");
+            binarytree.Display();
+            Console.ReadLine();
+        }
+    }
+}
