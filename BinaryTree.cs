9e5d70ae389a88bf41ea73e7b829fdbfb3b83b4a
Siddhant <santoshshrivastava39@gmail.com>
2022-03-24 23:28:34 +0530

    Create BST by adding 56 then 30 and 70

---
+++ BinaryTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+        T nodedata;
+        public BinaryTree<T> leftnode { get; set; }
+        public BinaryTree<T> rightnode { get; set; }
+        bool result = false;
+
+        public BinaryTree(T nodedata)                                  
+        {
+            this.nodedata = nodedata;
+            this.nodedata = nodedata;
+            this.leftnode = null;
+            this.rightnode = null;
+        }
+
+        public void Insert(T item)
+        {
+            T currentnodevalue = this.nodedata;
+            if ((currentnodevalue.CompareTo(item)) > 0)
+            {
+                if (this.leftnode == null)
+                    this.leftnode = new BinaryTree<T>(item);
+                else
+                    this.leftnode.Insert(item);
+            }
+            else
+            {
+                if (this.rightnode == null)
+                    this.rightnode = new BinaryTree<T>(item);
+                else
+                    this.rightnode.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.leftnode != null)
+            {
+                this.leftnode.Display();
+            }
+            Console.WriteLine(this.nodedata.ToString());
+            if (this.rightnode != null)
+            {
+                this.rightnode.Display();
+            }
+        }
+    }
+}
